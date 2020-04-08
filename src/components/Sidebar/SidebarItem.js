bf7b995ddc30ecd6d8362b6620e829d6450a724f
kishor neupane <kishorliv@gmail.com>
2020-04-08 22:42:47 +1000

    Update default export.

--- src/components/Sidebar/SidebarItem.js
+++ src/components/Sidebar/SidebarItem.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import classNames from 'classnames';
-// import './style.css';
-
-// const SidebarItem = (props) => {
-//     const { label, img, active } = {...props};
-//     return(
-//         <div className={active ? "sidenav-elem sidenav-elem-active" : "sidenav-elem"}>
-//             <i className={classNames(img, "")}></i>
-//             <span className="sidebar-label">{label}</span>
-//         </div>
-//     );
-// };
-
-// export default SidebarItem;
-
 import React from 'react';
 import classNames from 'classnames';
 import './style.css';
@@ -29,4 +13,4 @@ const SidebarItem = (props) => {
     );
 };
 
-export default SidebarItem;
+export { SidebarItem };
