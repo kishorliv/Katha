8b5b4bc4f500ca4d3eb7114ffd973b04c2e32876
kishor neupane <kishorliv@gmail.com>
2020-04-08 22:43:22 +1000

    Import using named export.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'; 
-import ReactDOM from "react-dom";
-import Layout from './components/Layout';
+import {Layout} from './components/Layout';
 
 const App = () => {
   return <div>
