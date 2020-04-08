07127d26a44da8e40eceb1d1c32b0cac15e5982a
kishor neupane <kishorliv@gmail.com>
2020-04-08 22:45:20 +1000

    Update import.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SidebarItem from './SidebarItem';
+import { SidebarItem } from './SidebarItem';
 import './style.css';
 
 
