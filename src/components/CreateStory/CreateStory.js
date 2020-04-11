390ebc5398cf764d1d0a16161568bb1e778cef8d
kishor neupane <kishorliv@gmail.com>
2020-04-11 21:30:34 +1000

    Add event handlers for different form elements.

--- src/components/CreateStory/CreateStory.js
+++ src/components/CreateStory/CreateStory.js
@@ -5,11 +5,15 @@ class CreateStory extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            title: ""
+            title: "",
+            description: "",
+            author: "",
+            tags: ""
         };
     }
 
     handleSubmit = (event) => {
+        // TODO: POST api call to add data
         console.log('Submitted.');
         event.preventDefault();
     };
@@ -20,9 +24,37 @@ class CreateStory extends React.Component{
         });
     };
 
+    handleDescriptionChange = (event) => {
+        this.setState({
+            description: event.target.value
+        });
+    };
+
+    handleAuthorChange = (event) => {
+        this.setState({
+            author: event.target.value
+        });
+    };
+
+    handleTagsChange = (event) => {
+        this.setState({
+            tags: event.target.value
+        });
+    };
+
     render(){
         return(
-            <Form handleSubmit={this.handleSubmit} handleTitleChange={this.handleTitleChange} />
+            <Form 
+                handleSubmit={this.handleSubmit} 
+                title={this.state.title}
+                handleTitleChange={this.handleTitleChange}
+                description={this.state.description}
+                handleDescriptionChange={this.handleDescriptionChange}
+                author={this.state.author}
+                handleAuthorChange={this.handleAuthorChange}
+                tags={this.state.tags}
+                handleTagsChange={this.handleTagsChange}
+                />
         );
     }
 }
