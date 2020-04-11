b477f04a6785b4e37841470890635226c9cf5998
kishor neupane <kishorliv@gmail.com>
2020-04-11 21:31:22 +1000

    Add input elements and take different props.

--- src/components/CreateStory/Form.js
+++ src/components/CreateStory/Form.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { Editor } from './Editor';
 
 
-const Form = ({ handleSubmit, title, handleTitleChange }) => {
-
+const Form = ({ 
+                handleSubmit,
+                title, 
+                handleTitleChange,
+                description,
+                handleDescriptionChange,
+                author,
+                handleAuthorChange,
+                tags,
+                handleTagsChange 
+}) => {
     return(
         <div>
             <form onSubmit={handleSubmit}>
@@ -11,11 +20,23 @@ const Form = ({ handleSubmit, title, handleTitleChange }) => {
                 <br />
                 <input type="text" value={title} onChange={handleTitleChange} />
                 <br /><br />
+                <label>Description</label>
+                <br />
+                <input type="text" value={description} onChange={handleDescriptionChange} />
+                <br /><br />
+                <label>Author</label>
+                <br />
+                <input type="text" value={author} onChange={handleAuthorChange} />
+                <br /><br />
                 <label>Write</label>
                 <div className="ckeditor">
                     <Editor />
                 </div>
+                <br /><br />
+                <label>Tags</label>
                 <br />
+                <input type="text" value={tags} onChange={handleTagsChange} />
+                <br /><br />
                 <input type="submit" value="Submit" />
             </form>
         </div>
