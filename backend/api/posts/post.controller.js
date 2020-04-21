82e23dd04c2d85be58c72f200853c14f3d6a3f03
kishor neupane <kishorliv@gmail.com>
2020-04-21 23:07:54 +1000

    Add model, service, controller and test for posts route.

--- backend/api/posts/post.controller.js
+++ backend/api/posts/post.controller.js
@@ -2,39 +2,44 @@ const express = require('express');
 const router = express.Router();
 
 const postService = require('./post.service');
-const postSchemaValidator = require('../../_middlewares/schema-validator');
-const postSchema = require('../../api/posts/post.validator');
 
 // routes
-router.get('/getAll', getAll);
-router.get('/:id', getById);
+router.get('/', getAll);
+router.get('/:postId', getById);
 router.get('/title/:title', getByTitle);
-router.post('/add', postSchemaValidator(postSchema), add);
+router.post('/create', create);
+router.patch('/:postId/update', updatePost);
 
 module.exports = router;
 
 
 function getAll(req, res, next){
     postService.getAll()
-    .then(posts => res.send({posts}))
+    .then(posts => res.send(posts))
     .catch(err => next(err));
 }
 
 function getById(req, res, next){
-    postService.getById(req.params.id)
-    .then(post => post ? res.send({post}) : res.status(404).send({message: 'Post not found!'}))
+    postService.getById(req.params.postId)
+    .then(post => post ? res.send(post) : res.status(404).send({message: 'Post not found!'}))
     .catch(err => next(err));  
 }
 
 function getByTitle(req, res, next){
     console.log('title: ', req.params.title);
     postService.getByTitle(req.params.title)
-    .then(post => post ? res.send({post}) : res.status(404).send({message: 'Post not found!'}))
+    .then(post => post ? res.send(post) : res.status(404).send({message: 'Post not found!'}))
     .catch(err => next(err));  
 }
 
-function add(req, res, next){
-    postService.add(req.body)
+function create(req, res, next){
+    postService.create(req.body)
     .then(post => post ? res.status(201).send(post) : res.status(400).send({message: 'Post cannot be created!'}))
     .catch(err => next(err));
+}
+
+function updatePost(req, res, next){
+    postService.updatePost(req.params.postId, req.body)
+    .then((post) => post ? res.status(200).send(post) : res.status(400).send({message: 'Post cannot be updated!'}))
+    .catch((err) => next(err));
 }
