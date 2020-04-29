0a7b25f1051b7483373a0c485f42ba84612956f3
kishor neupane <kishorliv@gmail.com>
2020-04-29 22:35:27 +1000

    Add Firebase class, context and auth apis.

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,44 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const devConfig = {
+    apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_DEV_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_DEV_FIREBASE_DB,
+    projectId: process.env.REACT_APP_DEV_FIREBASE_PRJ_ID,
+    storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_SENDER_ID,
+  };
+   
+const prodConfig = {
+    apiKey: process.env.REACT_APP_PROD_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_PROD_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_PROD_FIREBASE_DB,
+    projectId: process.env.REACT_APP_PROD_FIREBASE_PRJ_ID,
+    storageBucket: process.env.REACT_APP_PROD_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_PROD_FIREBASE_SENDER_ID,
+  };
+  
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(devConfig);
+    this.auth = app.auth();  // firebase's auth service
+  }
+
+  signUp = (email, password) => {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  logIn = (email, password) => {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logOut = () => {
+    this.auth.signOut();
+  };
+}
+   
+export { Firebase };
