1e48589d3f83ea427180e66be5774362152fd34e
kishor neupane <kishorliv@gmail.com>
2020-04-29 22:36:39 +1000

    Add FirebaseContext.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'; 
 import ReactDOM from "react-dom";
+import { HashRouter as Router } from 'react-router-dom'; 
+import App from './App';
+import { Firebase, FirebaseContext } from './components/Firebase';
 
 
 const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Router><App /></Router>, wrapper) : false;
+wrapper ? ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <Router>
+            <App />
+        </Router>
+    </FirebaseContext.Provider>
+    , wrapper) : false;
