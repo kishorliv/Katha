0800b83acfb2845a0268d79a851c8ab4b4ce3de8
kishor neupane <kishorliv@gmail.com>
2020-05-07 21:02:02 +1000

    Add route for PasswordForgetPage component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import {Layout} from './components/Layout';
 import { LogIn } from './components/LogIn';
 import { withAuthentication, AuthUserContext } from './components/Session';
+import { PasswordForgetPage } from "./components/PasswordForget";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         }
         </AuthUserContext.Consumer> 
         <Route exact path='/login' component={LogIn} />
+        <Route exact path='/pwd-forget' component={PasswordForgetPage} />
     </div>
   );
 };
