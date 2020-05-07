605582a00e6638f33d15383a52afcb85a6c010ee
kishor neupane <kishorliv@gmail.com>
2020-05-07 21:02:58 +1000

    Update firebase config and add verifyEmail and resetPassword methods.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,27 +1,15 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const devConfig = {
-    apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_DEV_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_DEV_FIREBASE_DB,
-    projectId: process.env.REACT_APP_DEV_FIREBASE_PRJ_ID,
-    storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_SENDER_ID,
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB,
+    projectId: process.env.REACT_APP_FIREBASE_PRJ_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   };
    
-const prodConfig = {
-    apiKey: process.env.REACT_APP_PROD_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_PROD_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_PROD_FIREBASE_DB,
-    projectId: process.env.REACT_APP_PROD_FIREBASE_PRJ_ID,
-    storageBucket: process.env.REACT_APP_PROD_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_PROD_FIREBASE_SENDER_ID,
-  };
-  
-const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
-
-
 class Firebase {
   constructor() {
     app.initializeApp(config);
@@ -30,15 +18,26 @@ class Firebase {
 
   signUp = (email, password) => {
     return this.auth.createUserWithEmailAndPassword(email, password);
-  }
+  };
 
   logIn = (email, password) => {
     return this.auth.signInWithEmailAndPassword(email, password);
-  }
+  };
 
   logOut = () => {
     this.auth.signOut();
   };
+
+  resetPassword = (email) => {
+    return this.auth.sendPasswordResetEmail(email);
+  };
+
+  verifyEmail = () => {
+    return this.auth.currentUser.sendEmailVerification({
+      url: REACT_APP_EMAIL_CONFIRMATION_REDIRECT_URL
+    })
+  };
+
 }
    
 export { Firebase };
