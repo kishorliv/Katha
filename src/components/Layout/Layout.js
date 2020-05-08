a5e3f3bf8608cb155686d90c32183f56fd06ed48
kishor neupane <kishorliv@gmail.com>
2020-05-08 20:00:31 +1000

    Wrap Layout component in LayoutPage and import MainArea.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,36 +1,26 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+
 import { Sidebar } from '../Sidebar';
-import { StoryList, StoryPreviewArea } from '../StoryList';
-import { CreateStory } from '../CreateStory';
 import './layout.scss';
 import Logo from '../../assets/icons/logo.png';
 import { LogOut } from '../LogOut';
 import { withEmailVerification } from '../Session';
+import MainArea from '../MainArea';
 
 
-const MainArea = () => {
-  return (
-    <div>
-      <Switch>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route exact path="/stories" component={StoryList} />
-        <Route path="/stories/:title" component={StoryPreviewArea} />
-        <Route path="/create" component={CreateStory} />
-        <Route exact path='/edit/:id' component={CreateStory} />
-        <Dashboard />
-      </Switch>
-    </div>
-  )
-}
-
-
-const Dashboard = () => {
+/**
+ * LayoutPage renders Layout component.
+ * It is the main or home page.
+ */
+const LayoutPage = () => {
   return(
-    <p>Dashboard Component</p>
+    <Layout />
   );
 };
 
+/**
+ * LayoutBase renders Sidebar, header and MainArea component.
+ */
 class LayoutBase extends React.Component {
   constructor(props) {
     super(props);
@@ -50,38 +40,39 @@ class LayoutBase extends React.Component {
 
     return (
       <div id='layout'>
-      <div id='left' className={leftOpen} >
-          <div className='icon'
-              onClick={this.toggleSidebar} >
-              &equiv;
-          </div>
-          <div className={`sidebar ${leftOpen}`} >
-              <div className='header'>
-                <h3 className='title'>
-                  Asmita Gaire
-                </h3>
-              </div>
-              <div className='sidebar-content'>
-                <Sidebar />
-              </div>
-          </div>
-      </div>
-      <div id='main'>
-          <div className='header'>
-              <img src={Logo} alt="" />
-              <div className='float-right m-auto'>
-                <LogOut />
-              </div>
-          </div>
-          <div className='content'>
+        <div id='left' className={leftOpen} >
+            <div className='icon'
+                onClick={this.toggleSidebar} >
+                &equiv;
+            </div>
+            <div className={`sidebar ${leftOpen}`} >
+                <div className='header'>
+                  <h3 className='title'>
+                    Asmita Gaire
+                  </h3>
+                </div>
+                <div className='sidebar-content'>
+                  <Sidebar />
+                </div>
+            </div>
+        </div>
+        <div id='main'>
+            <div className='header'>
+                <img src={Logo} alt="" />
+                <div className='float-right m-auto'>
+                  <LogOut />
+                </div>
+            </div>
+            <div className='content'>
               <MainArea />
-          </div>
+            </div>
+        </div>
       </div>
-  </div>
     );
   }
 }
 
 const Layout = withEmailVerification(LayoutBase);
 
+export default LayoutPage;
 export { Layout };
