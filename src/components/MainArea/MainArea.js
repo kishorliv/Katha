ac3015a471b9d2fd99bbd49d1006b80344985b90
fernandoBro <gauravpudel1990@gmail.com>
2020-05-09 23:07:09 +0545

    add redux provider and store

--- src/components/MainArea/MainArea.js
+++ src/components/MainArea/MainArea.js
@@ -6,6 +6,8 @@ import StoryPreviewArea from '../StoryPreviewArea';
 import { StoryList }from '../StoryList';
 import { CreateStory } from '../CreateStory';
 
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 
 /**
  * MainArea component exists besides the sidebar below header.
@@ -14,14 +16,16 @@ import { CreateStory } from '../CreateStory';
 const MainArea = () => {
     return (
       <div>
-        <Switch>
-          <Route path="/dashboard" component={DashboardArea} />
-          <Route exact path="/stories" component={StoryList} />
-          <Route path="/stories/:title" component={StoryPreviewArea} />
-          <Route path="/create" component={CreateStory} />
-          <Route exact path='/edit/:id' component={CreateStory} />
-          <DashboardArea />
-        </Switch>
+        <Provider store={store}>
+          <Switch>
+            <Route path="/dashboard" component={DashboardArea} />
+            <Route exact path="/stories" component={StoryList} />
+            <Route path="/stories/:title" component={StoryPreviewArea} />
+            <Route path="/create" component={CreateStory} />
+            <Route exact path='/edit/:id' component={CreateStory} />
+            <DashboardArea />
+          </Switch>
+        </Provider>
       </div>
     )
 }
