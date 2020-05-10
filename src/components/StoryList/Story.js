7527c2d001c6cd16e4685c79a95925e6da6ebcdf
kishor neupane <kishorliv@gmail.com>
2020-05-10 18:52:07 +1000

    Add linktoRead, linkToEdit and linkToDelete props.

--- src/components/StoryList/Story.js
+++ src/components/StoryList/Story.js
@@ -4,9 +4,9 @@ import './style.css';
 import { Card } from '../Card';
 
 
-const Story = ({ img, title, text, linkTo }) => {
+const Story = ({ img, title, text, linkToRead, linkToEdit, linkToDelete }) => {
     return(
-        <Card img={img} title={title} text={text} linkTo={linkTo} />
+        <Card img={img} title={title} text={text} linkToRead={linkToRead} linkToEdit={linkToEdit} linkToDelete={linkToDelete} />
     );
 };
 
