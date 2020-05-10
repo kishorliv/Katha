98cbfbc9d6b284de0445cbdc54d980fe3e9f5ba3
kishor neupane <kishorliv@gmail.com>
2020-05-11 01:01:59 +1000

    Call api to create the story.

--- src/components/CreateStory/CreateStory.js
+++ src/components/CreateStory/CreateStory.js
@@ -1,44 +1,104 @@
-import React from 'react';
-import { Form } from './Form';
+import React, { Fragment } from 'react';
+import axios from 'axios';
 
-class CreateStory extends React.Component{
+import { Form } from './Form';
+import { apiEndpoint } from '../../constants/urls';
+import { AuthUserContext } from '../Session';
+
+
+class CreateStoryBase extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             title: "",
             description: "",
-            author: "",
-            tags: "",
+            tags: [],
             content: "",
+            authId: "", //firebase auth id
+            userId: "", // mongoose user schema id 
+            error: null,
+            loading: false
         };
     }
 
 
     componentDidMount() {
-        const {id} = this.props.match.params;
-        if(id !== undefined){
-            console.log("edit"+id);
-            this.setState({
-                title: "Post1",
-                description: "This is a post to be edited",
-                author: "mahesh",
-                tags: "nepal blog",
-                content: "<h1>Welcome</h1><p>Hello Bye!</p>"
-            });
-        }
-        else{
-            console.log("CREATE");
-        }
+        this.setState({
+            authId: this.props.authUser.uid
+        });
+
+        // const {id} = this.props.match.params;
+        // if(id !== undefined){
+        //     console.log("edit"+id);
+        //     this.setState({
+        //         title: "Post1",
+        //         description: "This is a post to be edited",
+        //         author: "mahesh",
+        //         tags: "nepal blog",
+        //         content: "<h1>Welcome</h1><p>Hello Bye!</p>"
+        //     });
+        // }
+        // else{
+        //     console.log("CREATE");
+        // }
         //fetch api data and update state data accordingly
     }
 
 
     handleSubmit = (event) => {
+        const { authId, userId, title, description, content, tags, loading } = this.state;
+
+        this.setState({
+            loading: true
+        });
+
         // TODO: POST api call to add data
         //check this.props.location.pathname. If it contains edit then send PUT request, else send POST req.
+
+        // get user by authId first to send userId in the request
+        axios.get(apiEndpoint + '/users/authId/' + authId)
+             .then((user) => {
+                 console.log('Get user by authid response: ', user);
+                 this.setState({
+                     userId: user.data._id
+                 });
+
+                const postData = {
+                    title: title,
+                    description: description,
+                    contentHtml: '<p>get this content from somewhere maybe localstorage</p>',
+                    tags: ['google', 'people'],
+                    userId: user.data._id
+                }
+                console.log(postData);
+                axios.post(apiEndpoint + '/posts/create', postData)
+                    .then(() => {
+                        this.setState({
+                            loading: false
+                        });
+                        console.log('Story posted successfully.');
+                    })
+                    .catch((error) => {
+                        this.setState({
+                            error: error,
+                            loading: false
+                        });
+                        console.log('Post story error: ', error);
+                    });
+
+             })
+             .catch((error) => {
+                 this.setState({
+                     error: error,
+                     loading: false
+                 });
+                 console.log('Cannot fetch user by authId: ', error);
+             });
+
+
+
         event.preventDefault();
-        console.log('Submitted.');
-        console.log(this.state);
+        console.log('Submitted.')
     };
 
     handleChange = (e) => {
@@ -48,19 +108,31 @@ class CreateStory extends React.Component{
     }
 
     render(){
-        console.log(this.state);
+        const { loading, error, title, description, author, tags, content } = this.state;
+
         return(
-            <Form 
-                handleSubmit={this.handleSubmit} 
-                title={this.state.title}
-                description={this.state.description}
-                author={this.state.author}
-                tags={this.state.tags}
-                content={this.state.content}
-                handleChange={this.handleChange}
-                />
+            <Fragment>
+                <Form 
+                    handleSubmit={this.handleSubmit} 
+                    title={title}
+                    description={description}
+                    tags={(tags)}
+                    content={content}
+                    handleChange={this.handleChange}
+                    />
+                {loading && <p>Loading...</p>}
+                {error && <p>Sorry, could not create story.</p>}
+            </Fragment>
         );
     }
 }
 
+const CreateStory = (props) => (
+    <AuthUserContext.Consumer>
+    {authUser => <CreateStoryBase {...props} authUser={authUser} />}
+    </AuthUserContext.Consumer>
+);
+
 export { CreateStory };
+
+  
