55a471f7f4f4339f2f7ad642c25f03ac889eb753
kishor neupane <kishorliv@gmail.com>
2020-05-10 18:52:42 +1000

    Call api to get all the story posts.

--- src/components/StoryList/StoryList.js
+++ src/components/StoryList/StoryList.js
@@ -1,27 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 
 import { Story } from './Story';
+import { apiEndpoint } from '../../constants/urls';
 import LogoImage from '../../assets/icons/logo.png';
 
-
-const stories = [
-    {
-        "title": "this is a title",
-        "text": "lorem ipsum dolor amet what the fudge is this dolor emet amet corona oh my lorem"
-    },
-    {
-        "title": "comeon",
-        "text": "lorem ipsum dolor is this dolor emet amet corona oh my lorem lorem ipsum dolor is this dolor emet amet corona oh my lorem lorem ipsum dolor is this dolor emet amet corona oh my lorem"
-    },
-    {
-        "title": "this is a dtitle",
-        "text": "lorem ipsum dolor amet what the fudge is this dolor emet amet corona oh my lorem"
-    },
-    {
-        "title": "my titled",
-        "text": "lorem ipsum dolor is this dolor emet amet corona oh my lorem"
-    },
-];
 
 /**
  * Component that renders list of story cards.
@@ -31,42 +14,52 @@ class StoryList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            linkTo: [],
             stories: [],
-            imgs: []
+            imgs: [],
+            error: null,
+            loading: false
         };
     }
-
+    
     componentDidMount(){
-        // TODO: api call to fetch stories, might not need to fetch the whole html, just the props
-        // then the content can be fetched by sending GET to getPostByTitle method maybe, each title is unique
-        this.state.stories = []; //fill this
-        this.state.linkTo = this.state.stories.map((story) => `/stories/${story.title}`);
-        this.state.imgs = this.state.stories.map((story) => null); // find one image from the post or return a default image
+        this.setState({ loading: true})
+        // TODO: api call to fetch stories, might not need to fetch the whole html, just the required props
+        axios.get(apiEndpoint + '/posts/')
+             .then((stories) => {
+                 console.log(stories);
+                 this.setState({ 
+                     stories: stories.data,
+                     imgs: [], // find one image from the post or return a default image
+                     loading: false
+                    });
+             })
+             .catch((error) => {
+                 this.setState({ error: error});
+                 console.log('Fetch story error: ', error);
+             });
     }
 
     render(){
+        const { error, stories, loading } = this.state;
+
         return(
             <div className="container">
                 <div className="row">
-                    {/* { this.state.stories.map((story, i) => 
-                        <Story 
-                            img={this.state.imgs[i]} 
-                            title={story.title} 
-                            text={story.text} 
-                            linkTo={this.state.stories[i]} 
-                            key={stories.title} 
-                        />
-                    )} */}
-                    { stories.map((story, i) => 
-                        <Story 
-                            img={LogoImage} 
-                            title={story.title} 
-                            text={story.text} 
-                            linkTo={`/stories/${story.title}`} 
-                            key={story.title} 
-                        />
-                    )}
+                    { stories.map((story) => {
+                        return(
+                            <Story 
+                                img={LogoImage} 
+                                title={story.title} 
+                                text={story.description} 
+                                linkToRead={`/stories/${story.title}`} 
+                                linkToEdit={`/stories/${story.title}`}
+                                linkToDelete={`/stories/${story.title}`}
+                                key={story.title} 
+                            />
+                        );
+                    })}
+                    {loading && <p>Loading...</p>}
+                    {error && <p>Sorry, could not fetch stories.</p>}
                 </div>
             </div>
         );
