275c1bc003a2ab3d89abeb29d7ab6d0e07c075ce
kishor neupane <kishorliv@gmail.com>
2020-05-11 01:02:43 +1000

    Remove input field for author and add 'required' attribute.

--- src/components/CreateStory/Form.js
+++ src/components/CreateStory/Form.js
@@ -16,24 +16,20 @@ const Form = ({
             <form onSubmit={handleSubmit}>
                 <label>Title</label>
                 <br />
-                <input type="text" name='title' value={title} onChange={handleChange} />
+                <input type="text" name='title' value={title} onChange={handleChange} required />
                 <br /><br />
                 <label>Description</label>
                 <br />
-                <input type="text" name='description' value={description} onChange={handleChange} />
-                <br /><br />
-                <label>Author</label>
-                <br />
-                <input type="text" name='author' value={author} onChange={handleChange} />
+                <input type="text" name='description' value={description} onChange={handleChange} required />
                 <br /><br />
                 <label>Write</label>
                 <div>
-                    <Editor content={content} placeholder={'Start writing here...'}/>
+                    <Editor content={content} placeholder={'Start writing here...'} />
                 </div>
                 <br /><br />
                 <label>Tags</label>
                 <br />
-                <input type="text" name='tags' value={tags} onChange={handleChange} />
+                <input type="text" name='tags' value={tags} onChange={handleChange} required />
                 <br /><br />
                 <input type="submit" value="Submit" />
             </form>
